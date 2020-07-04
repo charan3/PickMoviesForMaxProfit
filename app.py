804bea2fb1620363a7027931667d999d5c02d6a6
Saicharan Reddy <credsai@amazon.com>
2020-07-04 18:21:15 +0530

    Implemented pickMoviesToMaximiseProfit api

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask, request
+from datetime import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/select_movies', methods=['POST'])
 def pick_movies_to_maximise_profit():
+    selected_movies = []
+    result = {}
     input_dict = request.json
-    print(input_dict)
-    """
-    TODO: To add core logic for the api
-    """
-    return input_dict
+    movies = input_dict['movies']
+    movies.sort(key=lambda movie: datetime.strptime(movie['end_date'], '%d %b %Y'))
+    if len(movies) != 0:
+        selected_movies.append(movies[0])
+        prev_end = movies[0]['end_date']
+        for i in range(1, len(movies)):
+            curr_start = movies[i]['start_date']
+            curr_end = movies[i]['end_date']
+            # checking overlapping movies
+            if datetime.strptime(curr_start, '%d %b %Y') > datetime.strptime(prev_end, '%d %b %Y'):
+                selected_movies.append(movies[i])
+                prev_end = curr_end
+
+    result['total_amount'] = len(selected_movies) * 1000
+    result['selected_movies'] = selected_movies
+    return result
 
 
 if __name__ == '__main__':
